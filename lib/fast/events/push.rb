9142a98cb68c93ecd2f8e0bad77a53356adcbc84
w00228948 <wulonghui@huawei.com>
2015-02-27 17:39:21 +0000

    Push to trigger build

--- lib/fast/events/push.rb
+++ lib/fast/events/push.rb
@@ -2,6 +2,12 @@ module Fast
   module Event
     class Push < Base
       def handle
+        ref = payload["ref"]
+        logger.info "Handle push, ref: #{ref}, pusher: #{payload["pusher"]}"
+        
+        git_branch = ref.gsub(/refs\/heads\//, '')
+        logger.info "Build #{job.name} with #{git_branch}"
+        jenkins_client.build_job(job.name, {:git_branch => git_branch})
       end
     end
   end
