24e57d4f2d075a71260804879463bfa7963b7377
w00228948 <wulonghui@huawei.com>
2015-02-28 14:06:43 +0000

    Add build history info and link

--- lib/fast/events/pull_request.rb
+++ lib/fast/events/pull_request.rb
@@ -4,13 +4,22 @@ module Fast
       def handle
         action = payload["action"]
         number = payload["number"]
-        pull_request = payload["pull_request"]
-        logger.info "Handle pull request, url: #{pull_request["html_url"]}, action: #{action}"
+        pull_request = payload["pull_request"] 
+        head = pull_request["head"] 
+        base = pull_request["base"] 
+        commit = "#{head["sha"][0..6]}(#{base["ref"]})"
+        committer = pull_request.fetch("user", {})["login"] || "unknown"
+        pr_head_branch = "pr/#{number}/head"
+        logger.info "Handle pull request, url: #{pull_request["html_url"]}, action: #{action}, commit: #{commit}, committer: #{committer}"
         
         unless action == "closed"
-          pr_head_branch = "pr/#{number}/head"
-          logger.info "Build #{job.name} with #{pr_head_branch}"
-          jenkins_client.build_job(job.name, {:git_branch => pr_head_branch})
+          logger.info "Build #{job.name}"
+          build_params = {
+            :git_branch => pr_head_branch,
+            :build_history_info => "PR/#{number} #{commit} by #{committer}",
+            :build_history_link => pull_request["html_url"]
+          }
+          jenkins_client.build_job(job.name, build_params)
         end
       end
     end
--- lib/fast/events/push.rb
+++ lib/fast/events/push.rb
@@ -3,11 +3,18 @@ module Fast
     class Push < Base
       def handle
         ref = payload["ref"]
-        logger.info "Handle push, ref: #{ref}, pusher: #{payload["pusher"]}"
-        
-        git_branch = ref.gsub(/refs\/heads\//, '')
-        logger.info "Build #{job.name} with #{git_branch}"
-        jenkins_client.build_job(job.name, {:git_branch => git_branch})
+        head_commit = payload["head_commit"]
+        pusher = payload["pusher"]["name"]   
+        git_branch = ref.gsub(/refs\/heads\//, '')        
+        logger.info "Handle push, ref: #{ref}, head_commit: #{head_commit["url"]}, pusher: #{pusher}"
+       
+        logger.info "Build #{job.name}"
+        build_params = {
+          :git_branch => git_branch,
+          :build_history_info => "Push #{head_commit["id"][0..6]}(#{git_branch}) by #{pusher}",
+          :build_history_link => URI.join(repository.url+"/", "commits/", git_branch).to_s
+        }
+        jenkins_client.build_job(job.name, build_params)
       end
     end
   end
